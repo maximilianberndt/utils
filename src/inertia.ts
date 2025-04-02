c56e59356127e46372fc904818de80f3a007079b
maximilianberndt <berndt.maximilian@gmail.com>
2025-04-02 11:47:28 +0200

    Add delta time compensation to inertia

--- src/inertia.ts
+++ src/inertia.ts
@@ -1,17 +1,13 @@
 export interface Inertia {
-  update: () => void
+  update: (deltaTime: number) => void
   add: (delta: number) => void
+  jump: (value: number) => void
   get: () => number
   setFriction: (friction: number) => void
 }
 
-export const inertia = ({
-  start = 0,
-  friction = 0.97,
-}: {
-  start: number
-  friction: number
-}): Inertia => {
+
+export const inertia = ({ start = 0, friction = 0.97 }): Inertia => {
   const data = {
     value: start,
     velocity: 0,
@@ -19,18 +15,28 @@ export const inertia = ({
   }
 
   // Update every frame
-  const update = () => {
-    data.velocity *= data.friction
-    data.value += data.velocity
+  const update = (dt = 1 / 60) => {
+    const timeAdjust = dt * 60
+    const frameFriction = Math.pow(data.friction, timeAdjust)
+
+    data.velocity *= frameFriction
+    data.value += data.velocity * timeAdjust
+  }
+
+  // Directly update the current value and set velocity to 0
+  const jump = (v = 0) => {
+    data.value = v
+    data.velocity = 0
   }
 
   return {
     update,
     get: () => data.value,
+    jump,
     add: (delta = 0) => {
       data.velocity += delta
     },
-    setFriction: (friction) => {
+    setFriction: (friction = 0.97) => {
       data.friction = friction
     },
   }
